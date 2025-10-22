3da9171f06ced305eb29e3b6288b09c62f8e435d
Uros Dimitrijevic <urosdim@icloud.com>
2025-10-22 22:38:12 +0200

    adding some new samples

--- Sample.fs
+++ Sample.fs
@@ -3,9 +3,13 @@ module FMutant.Sample
 
 
 let add a b = a + b
+
 let isLessThanConst x = x > -9
-let andOperator y z =  y && z
+let andOperator y z = y && z
 let alwaysTrue p = true || p
 let division r s = r / s
 let divisionByZero j = j / 0
 let multiplication t k = t * k
+let riskyDivision a = a / 1 + 0
+let survivorFunction x = x * 5 + 0
+let superWeakFunction x = x * 5
